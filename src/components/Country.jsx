604bd90d94c4fbb3d0b84a07e466c83b5ad8cace
Blank-ally <lia.stith@gmail.com>
2025-01-25 02:40:55 -0600

    init

---
+++ src/components/Country.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+function Country() {
+    const [name, setName] = useState("United States");
+    const [gold, setGold] = useState(0);
+  
+    function handleClick() {
+      // when a component's state is altered, it is re-rendered asynchronously by react
+      setGold(gold + 1);
+    }
+  
+    return (
+      <div>
+        Name: {name} 
+        <br></br>
+        Gold : {gold}
+        <br></br>
+        <button  onClick={handleClick}>Add Gold </button>
+
+      </div>
+    );
+  }
+  
+  export default Country;
