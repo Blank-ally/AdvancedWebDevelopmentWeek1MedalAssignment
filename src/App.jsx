69a08e9d59352952c097aac0547b61b87cec7ca5
Blank-ally <lia.stith@gmail.com>
2025-01-28 10:00:29 -0600

    add delete

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,53 @@
+import { useState } from "react";
 import Country from "./components/Country";
-import './App.css'
+import "./App.css";
 
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 function App() {
-  
+
+
+
+  const [countries, SetCountries ] = useState([
+    { id: 1, name: 'United States', gold: 2 },
+    { id: 2, name: 'China', gold: 3 },
+    { id: 3, name: 'France', gold: 0 },
+  ]);
+
+  function handleDelete(CountryID) {
+    console.log(`delete Country: ${CountryID}`);
+    SetCountries(countries.filter((c) => c.id !== CountryID));
+  }
+
+  function handleAddMedal(CountryID) {
+    console.log(`Add medal to Country: ${CountryID}`);
+    SetCountries(countries.filter((c) => c.id !== CountryID));
+  }
 
   return (
- <Country/>
-  )
-}
+    <div className="row">
 
-export default App
+      {countries.map((country) => (
+        <Country key={country.id} country={country} onDelete={handleDelete}  onAdd={handleAddMedal}/>
+      ))}
+
+    </div>
+  );
+}
+export default App;
+
+
+
+
+// import Country from "./components/Country";
+// import './App.css'
+
+// function App() {
+  
+
+//   return (
+//  <Country/>
+//   )
+// }
+
+// export default App
--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,23 +1,55 @@
-import { useState } from "react";
-function Country() {
-    const [name, setName] = useState("United States");
-    const [gold, setGold] = useState(0);
-  
-    function handleClick() {
-      // when a component's state is altered, it is re-rendered asynchronously by react
-      setGold(gold + 1);
-    }
-  
-    return (
-      <div>
-       Country Name: {name} 
-        <br></br>
-        Gold : {gold}
-        <br></br>
-        <button  onClick={handleClick}>Add Gold </button>
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+export default function Country(props) {
+  return (
 
-      </div>
-    );
-  }
+<div className="card m-2 col"  >
+  <div className="card-body">
+    <h5 className="card-title">{props.country.name}</h5>
+    <p className="card-text">
+      Gold medals: {props.country.gold}
+    </p>
   
-  export default Country;
+
+    <button onClick={() => props.onDelete(props.country.id)} className="btn btn-danger m-2">Delete </button>
+  </div>
+</div>
+
+
+  );
+}
+
+//  <button  onClick={() => props.onAdd(props.country.gold += 1)} className="btn btn-primary m-2">Add Medal </button>
+  //   <div
+  //     onClick={() => props.onDelete(props.country.id)}
+  //     >
+  //     {/* {props.country.name}
+  //     Gold : {gold}
+  //    */}
+
+     
+  // </div>
+
+// import { useState } from "react";
+// function Country() {
+//     const [name, setName] = useState("United States");
+//     const [gold, setGold] = useState(0);
+  
+//     function handleClick() {
+//       // when a component's state is altered, it is re-rendered asynchronously by react
+//       setGold(gold + 1);
+//     }
+  
+//     return (
+//       <div>
+//        Country Name: {name} 
+//         <br></br>
+//         Gold : {gold}
+//         <br></br>
+//         <button  onClick={handleClick}>Add Gold </button>
+
+//       </div>
+//     );
+//   }
+  
+//   export default Country;
